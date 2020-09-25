ac396951795ca5e29909af36847616ae5a9b24a2
yuria-saito <yuriaman@live.jp>
2020-09-25 18:16:06 +0900

    sp-home

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+
+//$(window).on('load', function(){
+  //$('#loading').fadeOut();
+});
+
+lottie.loadAnimation({
+  container: document.getElementById("loading-animation"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "loading-data.json"
+});
