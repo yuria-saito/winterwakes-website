ababd376dad6dac3728c304c58b2b4d4ca6a8368
yuria-saito <yuriaman@live.jp>
2020-09-29 10:36:13 +0900

    loadingが消える

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 // JavaScript Document
 
 
-//$(window).on('load', function(){
-  //$('#loading').fadeOut();
+$(window).on('load', function(){
+  $('#loading').fadeOut();
 });
 
-lottie.loadAnimation({
-  container: document.getElementById("loading-animation"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "loading-data.json"
-});
